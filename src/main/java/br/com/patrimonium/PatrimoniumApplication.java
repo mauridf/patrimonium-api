abadf8c3765946f795017d52b84c4cdf605b5947
mauridf <mauridf@gmail.com>
2026-03-02 15:42:27 -0300

    chore: bootstrap spring boot project structure
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/br/com/patrimonium/PatrimoniumApplication.java
@@ -0,0 +1,13 @@
+package br.com.patrimonium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrimoniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrimoniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/patrimonium/PatrimoniumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.patrimonium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimoniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
